8c1b2ff07058f941ff3bcb9413bc70c1f4f35883
Cjr37 <carlosclase37@gmail.com>
2023-11-29 18:14:20 +0100

    Creados componenetes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PadreComponent } from './componentes/padre/padre.component';
+import { HijoComponent } from './componentes/hijo/hijo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PadreComponent,
+    HijoComponent
   ],
   imports: [
     BrowserModule
